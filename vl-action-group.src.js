80e4167a0dc2c2116f58783fb37b762945a0b0a1
johanbuntinx <johan.buntinx@gmail.com>
2019-06-17 09:08:36 +0200

    ERK-1082: vl-ui-action-group web component maken

---
+++ vl-action-group.src.js
@@ -0,0 +1,79 @@
+import {VlElement} from '/node_modules/vl-ui-core/vl-core.js';
+
+
+/**
+ * VLActionGroup
+ * @class
+ * @classdesc Toon meerdere knoppen of links. De groep zorgt ervoor dat ze correct zijn uitgelijnd. <a href="demo/vl-action-group.html">Demo</a>.
+ *
+ * @extends VlElement
+ */
+export class VlActionGroup extends VlElement(HTMLElement) {
+  constructor() {
+    super(`
+      <style>
+        @import "../style.css";
+      </style>
+      <div class="vl-action-group">
+        <slot></slot>
+      </div>
+    `);
+  }
+
+  connectedCallback() {
+    this._applyAttributes();
+  }
+
+  get _classPrefix() {
+    return 'vl-action-group--';
+  }
+
+  static get _observedClassAttributes() {
+    return ['align','space-between','bordered'];
+  }
+
+  get _align() {
+    return this.getAttribute('align');
+  }
+
+  get _spaceBetween() {
+    return this.getAttribute('space-between');
+  }
+
+  get _bordered() {
+    return this.getAttribute('bordered');
+  }
+
+  _alignChangedCallback(oldValue, newValue){
+    if (["center", "right"].indexOf(newValue) >= 0){
+      this._element.classList.add(this._classPrefix + 'align-' + newValue);
+    }else {
+      this._element.classList.remove(this._classPrefix + 'align-' + oldValue);
+    }
+  }
+
+  _space_betweenChangedCallback(oldValue, newValue) {
+    if (newValue != undefined){
+      this._element.classList.add(this._classPrefix + 'space-between');
+    }else{
+      this._element.classList.remove(this._classPrefix + 'space-between');
+    }
+  }
+
+  _borderedChangedCallback(oldValue, newValue) {
+    if (newValue != undefined){
+      this._element.classList.add(this._classPrefix + 'bordered');
+    }else{
+      this._element.classList.remove(this._classPrefix + 'bordered');
+    }
+  }
+
+  _applyAttributes() {
+
+  }
+
+
+}
+
+
+customElements.define('vl-action-group', VlActionGroup);
