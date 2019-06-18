af21e247de105d24dd0a66e5aa73641f271c9192
johanbuntinx <johan.buntinx@gmail.com>
2019-06-18 10:46:11 +0200

    ERK-1082: vl-ui-action-group web component maken

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # webcomponent-vl-ui-action-group
 webcomponent-vl-ui-action-group  UI component van Webuniversum Vlaanderen als Web Component
+## Installatie
+```
+npm install --save vl-ui-action-group
+```
+
+## Demo
+```
+npm run demo
+```
+
+## Credits
+Zie de lijst van [ontwikkelaars](https://github.com/milieuinfo/webcomponent-vl-ui-action-group/graphs/contributors) die meegewerkt hebben aan de webcomponent.
+
+## Contact
+Mail je suggesties, opmerkingen of tips naar [help@omgevingvlaanderen.be](mailto:help@omgevingvlaanderen.be)
--- vl-action-group.src.js
+++ vl-action-group.src.js
@@ -31,40 +31,11 @@ export class VlActionGroup extends NativeVlElement(HTMLDivElement) {
     return ['align', 'space-between', 'bordered'];
   }
 
-  get _align() {
-    return this.getAttribute('align');
-  }
-
-  get _spaceBetween() {
-    return this.getAttribute('space-between');
-  }
-
-  get _bordered() {
-    return this.getAttribute('bordered');
-  }
-
   _alignChangedCallback(oldValue, newValue){
     if (["center", "right"].indexOf(newValue) >= 0){
       this._element.classList.add(this._classPrefix + 'align-' + newValue);
-    }else {
-      this._element.classList.remove(this._classPrefix + 'align-' + oldValue);
     }
-  }
-
-  _space_betweenChangedCallback(oldValue, newValue) {
-    this._attribuutChangedCallback('space-between', oldValue, newValue);
-  }
-
-  _borderedChangedCallback(oldValue, newValue) {
-    this._attribuutChangedCallback('bordered', oldValue, newValue);
-  }
-
-  _attribuutChangedCallback(attribuut, oldValue, newValue) {
-    if (newValue != undefined){
-      this._element.classList.add(this._classPrefix + attribuut);
-    }else{
-      this._element.classList.remove(this._classPrefix + attribuut);
-    }
+    this._element.classList.remove(this._classPrefix + 'align-' + oldValue);
   }
 
 }
